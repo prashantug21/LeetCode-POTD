8cd3977338c2f5eac16c78df90a16341b0db659d
Prashant <prashant.pyv@gmail.com>
2024-05-23 16:53:20 +0530

    potd 23-05-2024

---
+++ 23_05_2024.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int _beautifulSubsets(vector<int>& nums, int k, unordered_map<int, int>& freq, int i) {
+        if (i == nums.size()) {
+            return 1;
+        }
+        int result = _beautifulSubsets(nums, k, freq, i + 1); 
+        if (!freq[nums[i] - k]) { 
+            freq[nums[i]]++;
+            result += _beautifulSubsets(nums, k, freq, i + 1); 
+            freq[nums[i]]--;
+        }
+        return result;
+    }
+    
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        sort(nums.begin(), nums.end());
+        return _beautifulSubsets(nums, k, freq, 0) - 1;
+    }
+};
