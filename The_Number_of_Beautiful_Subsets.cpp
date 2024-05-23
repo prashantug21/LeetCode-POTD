8a8cdf191117dd4833b01217a661ed1e3dece7ad
Prashant <prashant.pyv@gmail.com>
2024-05-23 18:42:39 +0530

    Updated The Number of Beautiful Subsets

--- The_Number_of_Beautiful_Subsets.cpp
+++ The_Number_of_Beautiful_Subsets.cpp
@@ -1,22 +1,46 @@
 class Solution {
 private:
-    int _beautifulSubsets(vector<int>& nums, int k, unordered_map<int, int>& freq, int i) {
-        if (i == nums.size()) {
+    int solve(map<int,int>&mp,bool cond,vector<int>&v,int i){
+        if(i==v.size()){
             return 1;
         }
-        int result = _beautifulSubsets(nums, k, freq, i + 1); 
-        if (!freq[nums[i] - k]) { 
-            freq[nums[i]]++;
-            result += _beautifulSubsets(nums, k, freq, i + 1); 
-            freq[nums[i]]--;
+        int ans=0;
+        ans+=solve(mp,true,v,i+1);
+        if(cond){
+            ans+=(pow(2,mp[a[i]])-1)*solve(mp,false,v,i+1);
         }
-        return result;
+        return ans;
     }
     
 public:
-    int beautifulSubsets(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        sort(nums.begin(), nums.end());
-        return _beautifulSubsets(nums, k, freq, 0) - 1;
+    int beautifulSubsets(vector<int>& a, int k) {
+        int n=a.size();
+        sort(a.begin(),a.end());
+        vector<map<int,int>>ve(n);
+        vector<vector<int>>vec;
+        vector<int>dp(n,0);
+        map<int,int>con;
+        int m=0;
+        for(int i=0;i<n;i++){
+            if(mp.find(a[i]-k)!=mp.end() && mp.find(a[i])==mp.end()){
+                mp[a[i]]=mp[a[i]-k];
+                ve[mp[a[i]]][a[i]]++;
+                vec[mp[a[i]]].push_back(a[i]);
+            }else{
+                if(mp.find(a[i])==mp.end()){
+                    mp[a[i]]=m;
+                    ve[mp[a[i]]][a[i]]++;
+                    vec[mp[a[i]]].push_back(a[i]);
+                    m++;
+                }else{
+                    ve[mp[a[i]]][a[i]]++;
+                }
+            }
+        }
+        int ans=1;
+        for(int i=0;i<vec.size();i++){
+            ans*=solve(ve[i],true,vec[i],0);
+        }
+        return ans;
     }
 };
